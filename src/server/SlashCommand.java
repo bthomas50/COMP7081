aa600ad5d4f60a78158c530b9245e95f40cd1fce
Aequites <Aequites@K72F.ad.bcit.ca>
2014-11-05 15:39:39 -0800

    Bamboo Test

--- src/server/SlashCommand.java
+++ src/server/SlashCommand.java
@@ -1,3 +1,4 @@
+// Test Tag 1
 package server;
 
 import server.MsgHandlers.LogoutHandler;
@@ -187,11 +188,21 @@ public final class SlashCommand
                 }
                 break;
             case "/logout":
-                LogoutHandler.handle(pUser);
+                try
+                {
+                    LogoutHandler.handle(pUser);
+                }
+                catch(Exception ex)
+                { }
                 return E_CONSUMED;
             case "/whoisin":
-                WhoIsInHandler.handle(pUser);
-                break;
+                try
+                {
+                    WhoIsInHandler.handle(pUser);
+                }
+                catch(Exception ex)
+                { }
+                return E_CONSUMED;
             default:
                 s = "Server> Valid commands: /adduser /deluser /setrole /setteam /setcompany /t\n";
         }
